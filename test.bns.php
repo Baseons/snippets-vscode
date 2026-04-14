48914fa98981d2dfc9397155ed3222f46a0089c5
Warley Rodrigues de Moura <warleydev@hotmail.com>
2026-04-13 22:34:56 -0300

    save

--- test.bns.php
+++ test.bns.php
@@ -9,6 +9,24 @@
 
   <body>
 
+
+    <form  enctype="multipart/form-data" action="@route('web.form.create')" method="post">
+            <input type="hidden" name="_token" value="@csrf">
+            <input type="text" name="text" value="{{ old('text') }}" id="">
+            <input type="text" name="mult[a][b]" value="@old('mult.a.b')" id="">
+            <button type="submit">Send</button>
+        </form>
+
+
+
+
+
+        {{ dump(request()->old('text')) }}
+
+
+
+
+
     @include('view')
 
     @yield('name')
